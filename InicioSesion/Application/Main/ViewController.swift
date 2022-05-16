e8c2b590d6a28d388f0b4984de1aa413ee0476eb
Vicwell28 <vicwell.bj@gmail.com>
2022-05-16 15:45:21 -0500

    Facebooke

---
+++ InicioSesion/Application/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InicioSesion
+//
+//  Created by user215345 on 5/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
